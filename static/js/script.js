beb6ddcc361f287e380faf459055a44eac5bddf5
lbodlev888 <lbodlev888@gmail.com>
2025-07-13 15:45:35 +0300

    added dark mode

--- static/js/script.js
+++ static/js/script.js
@@ -40,3 +40,33 @@ function copyToClipboard() {
     }, 2000);
     });
 }
+function toggleDropdown() {
+      document.getElementById('themeDropdown').classList.toggle('hidden');
+    }
+
+function setTheme(mode) {
+    const html = document.documentElement;
+    localStorage.setItem('theme', mode);
+    if (mode === 'dark') {
+    html.classList.add('dark');
+    document.getElementById('theme-icon').textContent = '🌙';
+    } else if (mode === 'light') {
+    html.classList.remove('dark');
+    document.getElementById('theme-icon').textContent = '☀️';
+    } else {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        html.classList.add('dark');
+        document.getElementById('theme-icon').textContent = '🌙';
+    } else {
+        html.classList.remove('dark');
+        document.getElementById('theme-icon').textContent = '☀️';
+    }
+    }
+    document.getElementById('themeDropdown').classList.add('hidden');
+}
+
+// Apply saved or system theme
+(function () {
+    const savedTheme = localStorage.getItem('theme') || 'system';
+    setTheme(savedTheme);
+})();
