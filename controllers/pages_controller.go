8ac6206c77ef6fea82386fa82c7a5e41ab1d6b2a
Bodlev Laurentiu <lbodlev888@gmail.com>
2026-02-28 21:10:44 +0200

    added controller for html pages

--- controllers/pages_controller.go
+++ controllers/pages_controller.go
@@ -1,13 +1,34 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lbodlev888/url_shortener/services"
 )
 
 func IndexPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H{"title": "Home"})
+	raw_token, err := c.Cookie("token")
+	if err != nil {
+		c.Redirect(http.StatusTemporaryRedirect, "/login")
+		return
+	}
+
+	if !services.ValidateToken(raw_token) {
+		fmt.Println("invalid token")
+		c.Redirect(http.StatusTemporaryRedirect, "/login")
+		return
+	}
+
+	shorts, err := services.GetAllShorts(raw_token)
+	if err != nil {
+		fmt.Println(err)
+		c.HTML(http.StatusOK, "index.html", gin.H{"title": "Home"})
+		return
+	}
+
+	c.HTML(http.StatusOK, "index.html", gin.H{"title": "Home", "shorts": shorts})
 }
 
 func LoginPage(c *gin.Context) {
